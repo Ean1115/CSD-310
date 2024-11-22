dbec8187dccc5917683b094ca6f42c47909ba4bf
Ean1115 <reginak4490@gmail.com>
2024-11-22 14:24:34 -0600

    deleted
    deleted other files outside of zip folder

--- module-6/mysql_test.py
+++ module-6/mysql_test.py
@@ -1,31 +1,10 @@
-#Ean Masoner Module 6.2 Assignment
-import mysql.connector
-from mysql.connector import errorcode
+# city_functions.py
 
-config = {
-    "user": "localhost",
-    "password": "popcorn",
-    "host": "127.0.0.1",
-    "database": "movies",
-    "raise_on_warnings": True
-}
+def city_country(city, country):
+    return f"{city}, {country}"
 
-try:
-    db = mysql.connector.connect(**config)
-    print("Database user {user} connected to MYSQL on host {host} with database {database}".format(
-        user=config["user"], host=config["host"], database=config["database"]))
-
-    input("Press any key to continue...")
-
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("The supplied username or password are invalid")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("The specified database does not exist")
-    else:
-        print(err)
-
-finally:
-    db.close()
-
+# Calling the function three times
+print(city_country("Santiago", "Chile"))
+print(city_country("Paris", "France"))
+print(city_country("Tokyo", "Japan"))
 
